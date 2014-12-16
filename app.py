cb9b1f7e7b03f1d65ae1f3df16a947115ff88b84
digwtx <wtx358@qq.com>
2014-12-16 17:27:15 +0800

    项目初始化：添加主体代码

---
+++ app.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import json
+
+from flask import Flask, request, render_template, url_for
+
+from uploader import Uploader
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    """UEditor文件上传接口
+
+    config 配置文件
+    result 返回结果
+    """
+    result = {}
+    action = request.args.get('action')
+
+    # 解析JSON格式的配置文件
+    with open(os.path.join(app.static_folder, 'ueditor', 'php',
+                           'config.json')) as fp:
+        try:
+            # 删除 `/**/` 之间的注释
+            CONFIG = json.loads(re.sub(r'\/\*.*\*\/', '', fp.read()))
+        except:
+            CONFIG = {}
+
+    if action == 'config':
+        # 初始化时，返回配置文件给客户端
+        result = CONFIG
+
+    elif action in ('uploadimage', 'uploadfile', 'uploadvideo'):
+        # 图片、文件、视频上传
+        if action == 'uploadimage':
+            fieldName = CONFIG.get('imageFieldName')
+            config = {
+                "pathFormat": CONFIG['imagePathFormat'],
+                "maxSize": CONFIG['imageMaxSize'],
+                "allowFiles": CONFIG['imageAllowFiles']
+            }
+        elif action == 'uploadvideo':
+            fieldName = CONFIG.get('videoFieldName')
+            config = {
+                "pathFormat": CONFIG['videoPathFormat'],
+                "maxSize": CONFIG['videoMaxSize'],
+                "allowFiles": CONFIG['videoAllowFiles']
+            }
+        else:
+            fieldName = CONFIG.get('fileFieldName')
+            config = {
+                "pathFormat": CONFIG['filePathFormat'],
+                "maxSize": CONFIG['fileMaxSize'],
+                "allowFiles": CONFIG['fileAllowFiles']
+            }
+
+        if fieldName in request.files:
+            field = request.files[fieldName]
+            uploader = Uploader(field, config, app.static_folder)
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = '上传接口出错'
+
+    elif action in ('uploadscrawl'):
+        # 涂鸦上传
+        fieldName = CONFIG.get('scrawlFieldName')
+        config = {
+            "pathFormat": CONFIG.get('scrawlPathFormat'),
+            "maxSize": CONFIG.get('scrawlMaxSize'),
+            "allowFiles": CONFIG.get('scrawlAllowFiles'),
+            "oriName": "scrawl.png"
+        }
+        if fieldName in request.form:
+            field = request.form[fieldName]
+            uploader = Uploader(field, config, app.static_folder, 'base64')
+            result = uploader.getFileInfo()
+        else:
+            result['state'] = '上传接口出错'
+
+    elif action in ('catchimage'):
+        config = {
+            "pathFormat": CONFIG['catcherPathFormat'],
+            "maxSize": CONFIG['catcherMaxSize'],
+            "allowFiles": CONFIG['catcherAllowFiles'],
+            "oriName": "remote.png"
+        }
+        fieldName = CONFIG['catcherFieldName']
+        if fieldName in request.form:
+            source = []
+        elif '%s[]' % fieldName in request.form:
+            source = request.form.getlist('%s[]' % fieldName)
+        _list = []
+        for imgurl in source:
+            uploader = Uploader(imgurl, config, app.static_folder, 'remote')
+            info = uploader.getFileInfo()
+            _list.append({
+                'state': info['state'],
+                'url': info['url'],
+                'original': info['original'],
+                'source': imgurl,
+            })
+        result['state'] = 'SUCCESS' if len(_list) > 0 else 'ERROR'
+        result['list'] = _list
+
+    else:
+        result['state'] = '请求地址出错'
+
+    print result
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
